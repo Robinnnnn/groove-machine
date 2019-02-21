b3e8cb870a4cf0269ba16dd794af17f77cb98038
Robinnnnn <robin@Robins-MBP.fios-router.home>
2019-02-21 02:03:11 -0500

    default redirect to login route

--- src/App/routes.js
+++ src/App/routes.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Router } from '@reach/router'
+import { Router, Redirect } from '@reach/router'
 import { Login, OAuth, PrivateRoute } from './Auth'
 import Playlist from './Playlist'
 
 const Routes = () => (
   <Router>
-    <Login path='login' default />
+    <Login path='login' />
     <OAuth path='oauth' />
     <PrivateRoute as={Playlist} path='playlist/:id' />
+    <Redirect noThrow from='*' to='login' />
   </Router>
 )
 
