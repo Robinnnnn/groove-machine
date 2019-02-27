0c3cf9a7cc09d91aceba37b5f5fbe78da89b4312
Robinnnnn <robin@Robins-MBP.fios-router.home>
2019-02-27 00:16:55 -0500

    fix edge case where player shows 0:60 instead of 1:00

--- src/util/index.js
+++ src/util/index.js
@@ -2,7 +2,6 @@ const _appendLeadingZero = n => n < 10 ? `0${n}` : n
 
 export const msToMinutesAndSeconds = ms => {
   const minutes = Math.floor(ms / 60000);
-  const seconds = ((ms % 60000) / 1000).toFixed(0);
+  const seconds = Math.floor(((ms % 60000) / 1000)).toFixed(0);
   return `${minutes}:${_appendLeadingZero(seconds)}`;
 }
-
