22a91625b3197a97eb405631f4a44dcffb2403e3
Robinnnnn <robin@Robins-MBP.fios-router.home>
2019-03-02 23:33:42 -0500

    useState to toggle highlight animation

--- src/App/Search/Form/index.js
+++ src/App/Search/Form/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form as FinalForm, Field } from 'react-final-form'
 import './Form.scss'
 
@@ -11,32 +11,40 @@ const onSubmit = async values => {
   console.log('submitting', values)
 }
 
-const Form = () => (
-  <FinalForm
-    onSubmit={onSubmit}
-    validate={validate}
-    render={({ handleSubmit, submitting, pristine }) => (
-      <form className='search-form' onSubmit={handleSubmit}>
-        <Field
-          name='playlist'
-          component='input'
-          placeholder='Enter a link to a Spotify playlist'
-          className='playlist-input'
-          spellCheck={false}
-        />
-        <label className='playlist-input-label' />
+const Form = () => {
+  const [highlighted, toggleHighlight] = useState(false)
 
-        {
-          // <div className='accepted-formats'>Accepted Formats</div>
-          // <div>
-          //   <button type='submit' disabled={submitting || pristine}>
-          //     Submit
-          //   </button>
-          // </div>
-        }
-      </form>
-    )}
-  />
-)
+  const labelClass = highlighted ? 'focus' : ''
+
+  return (
+    <FinalForm
+      onSubmit={onSubmit}
+      validate={validate}
+      render={({ handleSubmit, submitting, pristine }) => (
+        <form className='search-form' onSubmit={handleSubmit}>
+          <Field
+            name='playlist'
+            component='input'
+            placeholder='Enter a link to a Spotify playlist'
+            className='playlist-input'
+            spellCheck={false}
+            onFocus={() => toggleHighlight(!highlighted)}
+            onBlur={() => toggleHighlight(!highlighted)}
+          />
+          <label className={`playlist-input-label ${labelClass}`} />
+
+          {
+            // <div className='accepted-formats'>Accepted Formats</div>
+            // <div>
+            //   <button type='submit' disabled={submitting || pristine}>
+            //     Submit
+            //   </button>
+            // </div>
+          }
+        </form>
+      )}
+    />
+  )
+}
 
 export default Form
