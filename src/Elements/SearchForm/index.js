42501215d5b8dd0aaa7a42fed5ba90260d725646
Robinnnnn <rokim8@gmail.com>
2019-03-08 22:44:59 -0500

    autofocus on open!

--- src/Elements/SearchForm/index.js
+++ src/Elements/SearchForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { PureComponent, useState } from 'react'
 import { Form as FinalForm, Field } from 'react-final-form'
 import { navigate } from '@reach/router'
 import { ReactComponent as SendLogo } from './send.svg'
@@ -31,57 +31,75 @@ const SearchForm = ({ visible, ctaPosition }) => {
   )
 }
 
-const InputForm = ({
-  formProps,
-  visible,
-  ctaPosition,
-  toggleHighlight,
-  highlighted,
-  valid,
-  loadPlaylist
-}) => {
-  const { handleSubmit, dirty } = formProps
+class InputForm extends PureComponent {
+  componentDidUpdate(prevProps) {
+    // Autofocus if search bar is being opened
+    if (!prevProps.visible && this.props.visible) {
+      this.textInput.focus()
+    }
+  }
 
-  const labelClass = highlighted || visible ? 'focused' : ''
-  const validClass = valid ? 'valid' : ''
-  const invalidClass = !valid && dirty ? 'invalid' : ''
-  const positionClass = `cta-position-${ctaPosition}`
+  render() {
+    const {
+      formProps,
+      visible,
+      ctaPosition,
+      toggleHighlight,
+      highlighted,
+      valid,
+      loadPlaylist
+    } = this.props
 
-  return (
-    <form className={`search-form ${positionClass}`} onSubmit={handleSubmit}>
-      <div className='search-bar-container'>
-        <div className='input-container'>
-          <Field
-            autoFocus
-            name='playlist'
-            component='input'
-            placeholder='Paste a link to a Spotify playlist'
-            className='playlist-input'
-            spellCheck={false}
-            onFocus={() => toggleHighlight(true)}
-            onBlur={() => !valid && toggleHighlight(false)}
-          />
-          <label
-            className={`playlist-input-label ${labelClass} ${validClass} ${invalidClass}`}
-          />
-        </div>
-        <div className={`cta-container ${validClass}`} onClick={loadPlaylist}>
-          <SendLogo className='cta' />
-        </div>
-      </div>
+    const { handleSubmit, dirty } = formProps
 
-      <div
-        className={`form-footer-container ${highlighted &&
-          validClass} ${invalidClass}`}
-        onClick={loadPlaylist}
-      >
-        <div className='footer-content'>
-          {invalidClass ? "hmm, are you sure that's a real link?" : ''}
-          {highlighted && valid ? 'Load Playlist' : ''}
+    const labelClass = highlighted || visible ? 'focused' : ''
+    const validClass = valid ? 'valid' : ''
+    const invalidClass = !valid && dirty ? 'invalid' : ''
+    const positionClass = `cta-position-${ctaPosition}`
+
+    return (
+      <form className={`search-form ${positionClass}`} onSubmit={handleSubmit}>
+        <div className='search-bar-container'>
+          <div className='input-container'>
+            <Field
+              name='playlist'
+              render={({ input }) => (
+                <>
+                  <input
+                    ref={i => (this.textInput = i)}
+                    type='text'
+                    className='playlist-input'
+                    placeholder='Paste a link to a Spotify playlist'
+                    spellCheck={false}
+                    onFocus={() => toggleHighlight(true)}
+                    onBlur={() => !valid && toggleHighlight(false)}
+                    {...input}
+                  />
+                  <label
+                    className={`playlist-input-label ${labelClass} ${validClass} ${invalidClass}`}
+                  />
+                </>
+              )}
+            />
+          </div>
+          <div className={`cta-container ${validClass}`} onClick={loadPlaylist}>
+            <SendLogo className='cta' />
+          </div>
         </div>
-      </div>
-    </form>
-  )
+
+        <div
+          className={`form-footer-container ${highlighted &&
+            validClass} ${invalidClass}`}
+          onClick={loadPlaylist}
+        >
+          <div className='footer-content'>
+            {invalidClass ? "hmm, are you sure that's a real link?" : ''}
+            {highlighted && valid ? 'Load Playlist' : ''}
+          </div>
+        </div>
+      </form>
+    )
+  }
 }
 
 export default SearchForm
