14caf0a68f9fad0ae7b87a3432a783ce4fcdfdc7
Robinnnnn <rokim8@gmail.com>
2019-03-29 02:26:01 -0400

    tighten

--- src/App/Playlist/LoadedPlaylist/Main/PlaylistHeader/index.js
+++ src/App/Playlist/LoadedPlaylist/Main/PlaylistHeader/index.js
@@ -12,47 +12,51 @@ const PlaylistHeader = memo(({ playlist }) => {
   return (
     <div className='playlist-header-container'>
       <div className='content-container'>
-        <a
-          className='title'
-          href={playlist.external_urls.spotify}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          {playlist.name}
-        </a>
-        <div className='subtitle-container'>
-          <p className='description'>{playlist.description}</p>
+        <div className='info-container'>
+          <div className='title-container'>
+            <a
+              className='title'
+              href={playlist.external_urls.spotify}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {playlist.name}
+            </a>
+          </div>
+          <div className='subtitle-container'>
+            <p className='description'>{playlist.description}</p>
+          </div>
+          <div className='stats-container'>
+            <p className='tracks'>Tracks: {playlist.tracks.total}</p>
+            <p className='followers'>Followers: {playlist.followers.total}</p>
+            <p className='duration'>Duration: {duration}</p>
+            {shouldDisplayAuthor ? (
+              <p className='author'>
+                Producer:{' '}
+                <a
+                  href={playlist.owner.external_urls.spotify}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {playlist.owner.display_name}
+                </a>
+              </p>
+            ) : null}
+          </div>
         </div>
-        <div className='stats-container'>
-          <p className='tracks'>Tracks: {playlist.tracks.total}</p>
-          <p className='followers'>Followers: {playlist.followers.total}</p>
-          <p className='duration'>Duration: {duration}</p>
-          {shouldDisplayAuthor ? (
-            <p className='author'>
-              Producer:{' '}
-              <a
-                href={playlist.owner.external_urls.spotify}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                {playlist.owner.display_name}
-              </a>
-            </p>
-          ) : null}
+        <div className='playlist-cover-container'>
+          <a
+            href={playlist.external_urls.spotify}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              className='playlist-cover'
+              src={playlist.images[0].url}
+              alt='cover'
+            />
+          </a>
         </div>
-      </div>
-      <div className='playlist-cover-container'>
-        <a
-          href={playlist.external_urls.spotify}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <img
-            className='playlist-cover'
-            src={playlist.images[0].url}
-            alt='cover'
-          />
-        </a>
       </div>
     </div>
   )
