77a2717d1b3df49fce2f0ea293be3a820473beb4
Robinnnnn <rokim8@gmail.com>
2019-03-29 02:09:05 -0400

    artwork, with some links

--- src/App/Playlist/LoadedPlaylist/Main/PlaylistHeader/index.js
+++ src/App/Playlist/LoadedPlaylist/Main/PlaylistHeader/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import { msToTimestamp } from 'util/index'
 import './PlaylistHeader.scss'
 
-const PlaylistHeader = ({ playlist }) => {
+const PlaylistHeader = memo(({ playlist }) => {
   const duration = msToTimestamp(
     playlist.tracks.items.reduce((d, i) => d + i.track.duration_ms, 0)
   )
@@ -11,31 +11,52 @@ const PlaylistHeader = ({ playlist }) => {
 
   return (
     <div className='playlist-header-container'>
-      <p className='title'>{playlist.name}</p>
-      <div className='subtitle-container'>
-        <p className='description'>{playlist.description}</p>
-        {shouldDisplayAuthor ? (
-          <>
-            <div className='dot' />
-            <a
-              className='author'
-              href={playlist.owner.external_urls.spotify}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {playlist.owner.display_name}
-            </a>
-          </>
-        ) : null}
+      <div className='content-container'>
+        <a
+          className='title'
+          href={playlist.external_urls.spotify}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {playlist.name}
+        </a>
+        <div className='subtitle-container'>
+          <p className='description'>{playlist.description}</p>
+        </div>
+        <div className='stats-container'>
+          <p className='tracks'>Tracks: {playlist.tracks.total}</p>
+          <p className='followers'>Followers: {playlist.followers.total}</p>
+          <p className='duration'>Duration: {duration}</p>
+          {shouldDisplayAuthor ? (
+            <p className='author'>
+              Producer:{' '}
+              <a
+                href={playlist.owner.external_urls.spotify}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                {playlist.owner.display_name}
+              </a>
+            </p>
+          ) : null}
+        </div>
       </div>
-      <div className='stats-container'>
-        <p className='tracks'>Tracks: {playlist.tracks.total}</p>
-        <p className='followers'>Followers: {playlist.followers.total}</p>
-        <p className='duration'>Duration: {duration}</p>
+      <div className='playlist-cover-container'>
+        <a
+          href={playlist.external_urls.spotify}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img
+            className='playlist-cover'
+            src={playlist.images[0].url}
+            alt='cover'
+          />
+        </a>
       </div>
     </div>
   )
-}
+})
 
 PlaylistHeader.propTypes = {
   playlist: PropTypes.shape({}).isRequired
