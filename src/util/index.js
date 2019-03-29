e4486cb1a691617ba7711b690581932d63719d63
Robinnnnn <rokim8@gmail.com>
2019-03-29 01:08:03 -0400

    update timestamp calculator to include hours

--- src/util/index.js
+++ src/util/index.js
@@ -1,9 +1,12 @@
 const _appendLeadingZero = n => (n < 10 ? `0${n}` : n)
 
-export const msToMinutesAndSeconds = ms => {
-  const minutes = Math.floor(ms / 60000)
-  const seconds = Math.floor((ms % 60000) / 1000).toFixed(0)
-  return `${minutes}:${_appendLeadingZero(seconds)}`
+export const msToTimestamp = ms => {
+  const h = Math.floor(ms / 60000 / 60)
+  const m = Math.floor((ms / 60000) % 60)
+  const s = Math.floor((ms % 60000) / 1000).toFixed(0)
+  const t = [m, s]
+  if (h) t.unshift(h)
+  return t.map(_appendLeadingZero).join(':')
 }
 
 export { log } from './logger'
